2dee5397527aa9d4ccb6e87b8a5a83c40e06482a
hadwin.jiang <hadwinjiang@126.com>
2019-04-10 17:06:21 +0800

    coryhouse redux5 link provider

--- coryhouse-react/src/index.js
+++ coryhouse-react/src/index.js
@@ -4,14 +4,20 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import configureStore from "./redux/configureStore";
 
 // function Hi() {
 //     // debugger;
 //     return <p>Hi.</p>;
 // }
 
+const store = configureStore();
+
 render(
-    <Router>
-        <App />
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
     , document.getElementById("app"));
